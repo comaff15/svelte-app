e7dd1b981c9644368ba8b792a1b45f22d0365d35
Gleb <zavarykingleb@gmail.com>
2024-06-11 17:17:05 +0400

    Add settings

---
+++ src/routes/settings/+page.server.js
@@ -0,0 +1,34 @@
+import {error, fail, redirect} from '@sveltejs/kit';
+import * as api  from '$lib/utils/api.js'
+
+export const load = ({locals}) => {
+    if (!locals.user) redirect(302, '/login')
+}
+
+export const actions = {
+    logout: async ({cookies, locals}) => {
+        cookies.delete('jwt', {path: '/'});
+        locals.user = null;
+    },
+    
+    save: async ({cookies, locals, request}) => {
+        if (!locals.user) error(401);
+
+        const data = await request.formData();
+
+        const user = {
+            username: data.get('username'),
+            email: data.get('email'),
+            password: data.get('password'),
+            bio: data.get('bio')
+        };
+
+        const body = await api.put('user', {user}, locals.user.token);
+        if(body.errors) return fail(400, body.errors);
+
+        const value = btoa(JSON.stringify(body.user));
+        cookies.set('jwt', value, {path: '/'});
+
+        locals.user = body.user;
+    }
+};
---
+++ src/routes/settings/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+    import {enchance} from '$app/forms';
+
+    import ListError from '../../lib/components/common/ListError.svelte';
+
+    /** @type {import('./$types').PageData} */
+    export let data;
+
+    /** @type {import('./$types').ActionData} */
+    export let form;
+</script>
+
+<svelte:head>
+    <title>Настройки</title>
+</svelte:head>
