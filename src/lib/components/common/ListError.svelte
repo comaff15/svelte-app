ab23e7ccdf34a64c208a62ea16e402b31f5f516a
Gleb <zavarykingleb@gmail.com>
2024-06-11 17:14:55 +0400

    added error for settings

---
+++ src/lib/components/common/ListError.svelte
@@ -0,0 +1,11 @@
+<script>
+    export let errors;
+</script>
+
+{#if errors}
+    <ul class = "error">
+        {#each Object.keys(errors) as key}
+            <li>{key} {errors[key]}</li>
+        {/each}
+    </ul>
+{/if}
