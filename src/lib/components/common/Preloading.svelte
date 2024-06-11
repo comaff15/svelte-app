2c00f427fa036e98a37835aa53cdb537bed0a20a
Gleb <zavarykingleb@gmail.com>
2024-06-11 17:16:21 +0400

    add preloading

---
+++ src/lib/components/common/Preloading.svelte
@@ -0,0 +1,68 @@
+<script>
+    import { onMount } from "svelte";
+
+    let step = 0;
+    let visible = false
+
+    onMount(() => {
+        visible = true;
+
+        const next = () => {
+            p += 0.1;
+            const remaining = 1 - step;
+
+            if(remaining > 0.15) setTimeout(next, 500 / remaining);
+        }
+
+        setTimeout(next, 250)
+    });
+</script>
+
+{#if visible}
+    <article class = "progress-container">
+        <section class = "progress" style="width: {step * 100}%;"/>
+    </article>
+{/if}
+
+{#if step >= 0.4}
+    <section class = "fade"/>
+{/if}
+
+<style>
+	.progress-container {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 4px;
+		z-index: 999;
+	}
+
+	.progress {
+		position: absolute;
+		left: 0;
+		top: 0;
+		height: 100%;
+		background-color: #5cb85c;
+		transition: width 0.4s;
+	}
+
+	.fade {
+		position: fixed;
+		width: 100%;
+		height: 100%;
+		background-color: rgba(255, 255, 255, 0.3);
+		pointer-events: none;
+		z-index: 998;
+		animation: fade 0.4s;
+	}
+
+	@keyframes fade {
+		from {
+			opacity: 0;
+		}
+		to {
+			opacity: 1;
+		}
+	}
+</style>
