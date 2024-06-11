07fe0ba33476a044a5d19f15761ab84cc3f38e21
Gleb <zavarykingleb@gmail.com>
2024-06-11 23:35:46 +0400

    Добавил ошибку

---
+++ src/routes/+error.svelte
@@ -0,0 +1,22 @@
+<script>
+    import {page} from '$app/stores'
+</script>
+
+<svelte:head>
+    <title>{$page.status}</title>
+</svelte:head>
+
+<article>
+    {#if $page.status === 404}
+        <h1>Страница не найдена</h1>
+    {:else}
+        <h1>Что-то пошло не так</h1>
+    {/if}
+</article>
+
+<style>
+    h1 {
+        margin: 4em 0;
+        text-align: center;
+    }
+</style>
