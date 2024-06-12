1bfcdfafa37720d91f23516499d3e75eb9f26fcf
Gleb <zavarykingleb@gmail.com>
2024-06-12 19:48:44 +0400

    rewrite some shit

--- src/lib/components/common/Header.svelte
+++ src/lib/components/common/Header.svelte
@@ -1,12 +1,6 @@
 <script>
     import {page} from '$app/stores'
 </script>
-<head>
-    <!-- meta tags -->
-    <meta charset="utf-8">
-    <title>FunnyCraft - ИГРОВОЙ СЕРВЕР MINECRAFT</title>
-  </head>
-
     <div class="container">
       <div class="clear-fix-container">
 
@@ -15,7 +9,7 @@
             <img src="./assets/img/logo.png" class="logoType" alt="hexworld logotype">
           </div>
           <div class="col d-flex justify-content-end align-items-center">
-              <a class="btn btn-hex-gray mr-2 btn-lg start" href=""><i class="fas fa-play"></i> Скачать лаунчер</a>
+              <a class="btn btn-hex-gray mr-2 btn-lg start" href="/launcher"><i class="fas fa-play"></i> Скачать лаунчер</a>
             </div>
         </div>
 
@@ -25,12 +19,36 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
               <div class="navbar-nav mr-auto">
-                <button on:click={() => window.location.href='/'} class="nav-item nav-link">Главная</button> 
-                <button on:click={() => window.location.href='/news'} class="nav-item nav-link">Новости</button> 
-                <button on:click={() => window.location.href='login'} class="nav-item nav-link">Войти</button>
-                <button on:click={() => window.location.href='/profile'} class="nav-item nav-link">Профиль</button> 
-                <button on:click={() => window.location.href='/register'} class="nav-item nav-link">Регистрация</button> 
-                <button on:click={() => window.location.href='/login'} class="nav-item nav-link">Войти</button>  
+                <ul class="navbar-nav">
+                  <li class="nav-item" aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+                    <a class="nav-link" class:active = { $page.url.pathname === '/'} href="/">
+                      Главная
+                    </a>
+                  </li>
+                  <li class="nav-item" aria-current={$page.url.pathname === '/news' ? 'page' : undefined}>
+                    <a class="nav-link" class:active = {$page.url.pathname === '/news'} href = "/news">
+                      Новости
+                    </a>
+                  </li>
+                  {#if $page.data.user}
+                    <li class="nav-item">
+                      <a class="nav-link" href = "/profile/@{$page.user.username}">
+                        {$page.user.username}
+                      </a>
+                    </li>
+                  {:else}
+                    <li class="nav-item" aria-current={$page.url.pathname === '/register' ? 'page' : undefined}>
+                      <a class="nav-link" class:active = {$page.url.pathname === '/register'} href = "/register">
+                          Регистрация
+                      </a>
+                    </li>
+                    <li class="nav-item" aria-current={$page.url.pathname === '/login' ? 'page' : undefined}>
+                      <a class="nav-link" class:active = {$page.url.pathname === '/login'} href = "/login">
+                          Войти
+                      </a>
+                    </li>
+                  {/if}
+                </ul>
               </div>
             </div>
         </nav>
@@ -82,7 +100,7 @@ nav.navbar-hex {
     height: 60px;
 }
 
-.navbar-nav > .nav-item.nav-link {
+.navbar-nav > .nav-item{
     padding: 15px;
     margin-left: -2px;
     border-left: 2px solid #2e2e2e;
